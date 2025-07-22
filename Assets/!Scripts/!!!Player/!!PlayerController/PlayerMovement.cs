8ccb6e69fc1613a7ef592b1aa01b70c209fe7ce3
crazed6 <creed2793@gmail.com>
2025-07-21 21:38:01 -0600

    fix

--- Assets/!Scripts/!!!Player/!!PlayerController/PlayerMovement.cs
+++ Assets/!Scripts/!!!Player/!!PlayerController/PlayerMovement.cs
@@ -673,7 +673,7 @@ public class CharacterController : MonoBehaviour
                             if (dmg != null && dashDamageProfile != null)
                             {
                                 DamageData dashDamage = new DamageData(gameObject, dashDamageProfile);
-                                dmg.TakeDamage2(dashDamage);
+                                dmg.TakeDamage(dashDamage.profile.damageAmount, gameObject);
                             }
                             else
                             {
