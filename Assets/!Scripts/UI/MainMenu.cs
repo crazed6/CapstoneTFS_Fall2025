d33fad65154e9f81141e543b58718a361baa6520
Kassie@00GundamRizer <Kassie35dev@Gmail.com>
2025-09-09 01:20:43 -0400

    updated changes to the ui, added dislay button for resolution and quality, title added to main menu, small changes to main menu ui and options menu in progress.

--- Assets/!Scripts/UI/MainMenu.cs
+++ Assets/!Scripts/UI/MainMenu.cs
@@ -6,7 +6,10 @@ public class MainMenu : MonoBehaviour
 {
 
     [Header("UI Panels")]
+    public GameObject mainMenu; // Reference to the main menu panel
     public GameObject savedGameFilesPanel; // Reference to the panel for saved game files
+    public GameObject creditsPanel; // Reference to the panel for credits
+    public GameObject creditsVideo; // Reference to the credits video
 
 
     public void Start()
@@ -26,7 +29,8 @@ public class MainMenu : MonoBehaviour
     }
     public void Settings() //method to start the first game scene
     {
-        SceneManager.LoadSceneAsync("Kadeem_SettingsMenu"); //Replace SampleScene with the name of the first game scene 
+        SceneManager.LoadSceneAsync("Kadeem_OptionsMenu"); //Replace SampleScene with the name of the first game scene
+        creditsVideo.SetActive(false);
         SoundManager.Instance.PlayBGM(1);
         Debug.Log("Settings button clicked");
     }
@@ -55,6 +59,8 @@ public class MainMenu : MonoBehaviour
         else
         {
           savedGameFilesPanel.SetActive(true);
+          mainMenu.SetActive(false);
+          creditsVideo.SetActive(false);
         } 
     }
 
@@ -62,7 +68,23 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Kadeem_MainMenu");
         savedGameFilesPanel.SetActive(false);
+        mainMenu.SetActive(true);
+        creditsVideo.SetActive(false);
         //SoundManager.Instance.PlayBGM(1);
+    }
+
+    public void OpenCreditsPanel()
+    {
+        if(creditsPanel == null)
+        {
+          Debug.LogError("CreditsPanel is not assigned in UIManager."); 
+        }
+        else
+        {
+          creditsPanel.SetActive(true);
+          creditsVideo.SetActive(true);
+          mainMenu.SetActive(false);
+        }
     }
 
 }
